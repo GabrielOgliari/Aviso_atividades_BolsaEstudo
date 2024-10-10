eed310614f6fbd989be85f2d52ca2328b550d046
GabrielOgliari <125907742+GabrielOgliari@users.noreply.github.com>
2024-10-10 19:31:01 -0300

    Ajustes para enviar link

--- aviso.py
+++ aviso.py
@@ -11,62 +11,56 @@ CHAT_ID = '5782098350'
 
 # URL a ser monitorada
 URL = 'https://eventos.unochapeco.edu.br/eventos/'
+URL = 'https://eventos.unochapeco.edu.br/Auxiliar/RetornarListaEventoWhiteLabel'
 
 # Payload JSON para a requisição POST
 payload = {
     "model": "{\"cidade\":null,\"filtroCampoTexto\":\"PUG\",\"ano\":null,\"unidade\":null,\"formatoEvento\":\"\"}"
 }
 
-# Caminho do arquivo onde as respostas já enviadas serão armazenadas
-STORAGE_FILE = 'notified_responses.json'
-
-# Função para carregar as respostas já notificadas
-def load_notified_responses():
-    if os.path.exists(STORAGE_FILE):
-        with open(STORAGE_FILE, 'r') as file:
-            try:
-                return json.load(file)
-            except json.JSONDecodeError:
-                # Se o arquivo estiver vazio ou corrompido, retorna uma lista vazia
-                return []
-    return []
-
-# Função para salvar as respostas já notificadas
-def save_notified_response(response_text):
-    notified_responses = load_notified_responses()
-    notified_responses.append(response_text)
-    with open(STORAGE_FILE, 'w') as file:
-        json.dump(notified_responses, file)
+def gravar_log(id):
+    with open('log.txt', 'a') as log_file:
+        log_file.write(f'{id}\n')
 
 # Função para verificar a presença do texto específico nos resultados da resposta
 def verificar_site():
     response = requests.post(URL, json=payload)
     if response.status_code == 200:
         response_text = response.text
-        notified_responses = load_notified_responses()
 
-        # Verifica se a resposta já foi notificada
-        if response_text not in notified_responses:
-            if "PUG" in response_text:  # Verifica se o texto "PUG" está na resposta
-                save_notified_response(response_text)
-                return response_text  # Retorna o texto completo encontrado
-    return None
+        response_json = json.loads(response_text)
+        objects = response_json['Object']
+
+        for obj in objects:
+            id = obj['id']
+            if "PUG" in obj['titulo']:
+                if os.path.exists('log.txt'):
+                    with open('log.txt', 'r') as log_file:
+                        log = log_file.read()
+                    if str(id) in log:
+                        continue
+                gravar_log(id)
+                return obj['url']
+
+    # return None
 
 # Função para enviar uma mensagem no Telegram
 async def send_message_to_telegram(text):
     try:
         bot = Bot(token='8069837006:AAFhgqqv0SNkzUDLgBEgpRKFAy_Ev5WR59A')
-        await bot.send_message(chat_id='-4503091244', text='Nova atividade encontrada!')
+        await bot.send_message(chat_id='-4503091244', text=text)
         return True
     except Exception as e:
         print(f'Erro ao enviar mensagem para o telegram: {e}')
 
 async def main():
     while True:
-        texto_encontrado = verificar_site()
-        if texto_encontrado:
-            await send_message_to_telegram(f'O texto "PUG" foi encontrado no site! Texto completo:\n\n{texto_encontrado}')
-        time.sleep(300)  # Espera 5 minutos antes de verificar novamente
+        url = verificar_site()
+        if url:
+            link = f'https://eventos.unochapeco.edu.br/{url}'
+            await send_message_to_telegram(f'nova atividade encontrada:\n\n{link}')
+        # time.sleep(300)  # Espera 5 minutos antes de verificar novamente
+
 
 if __name__ == '__main__':
     asyncio.run(main())
